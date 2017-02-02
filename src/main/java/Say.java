442d6bb38a99dcf56a3275e2e9fb9a9fe2fb4852
Ronak Shah <rshah@foundationmedicine.com>
2017-02-01 23:36:17 -0500

    Initial Commit

---
+++ src/main/java/Say.java
@@ -0,0 +1,6 @@
+public class Say {
+
+    public String getSaying(Sayings s, int i){
+        return s.sayingsList.get(i);
+    }
+}
---
+++ src/main/java/Sayings.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Sayings {
+    public ArrayList<String> sayingsList;
+
+    public Sayings() {
+        sayingsList = new ArrayList<String>();
+        sayingsList.add("sayings zero");
+        sayingsList.add("sayings one");
+        sayingsList.add("sayings two");
+        sayingsList.add("sayings three");
+
+    }
+}
